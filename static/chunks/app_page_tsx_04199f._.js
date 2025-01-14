be4a1cc5355498eca618c4b5b7d9265e6b61ce17
SUCHISMITA DUTTA <115866964+crack-head22@users.noreply.github.com>
2025-01-14 19:08:14 +0530

    Add files via upload

---
+++ static/chunks/app_page_tsx_04199f._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_tsx_04199f._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_tsx_04199f._.js",
+  "chunks": [
+    "static/chunks/node_modules_584b48._.js",
+    "static/chunks/_adf57c._.js"
+  ],
+  "source": "dynamic"
+});
